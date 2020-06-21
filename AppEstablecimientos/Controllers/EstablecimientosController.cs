d771c8586c2a95ea11f982fd67ae75f543ecd14b
César Jamit <cjc69371@udelasalle.edu.mx>
2020-06-20 19:04:29 -0600

    Agregar archivos de proyecto.

---
+++ AppEstablecimientos/Controllers/EstablecimientosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppEstablecimientos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AppEstablecimientos.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EstablecimientosController : ControllerBase
+    {
+        // GET: api/<EstablecimientosController>
+        [HttpGet]
+        public List<Establecimientos> Get()
+        {
+            return new Establecimientos().GetAll(); 
+        }
+
+        // GET api/<EstablecimientosController>/5
+        [HttpGet("{id}")]
+        public Establecimientos Get(int id)
+        {
+            return new Establecimientos().Get(id);
+        }
+
+        // POST api/<EstablecimientosController>
+        [HttpPost]
+        public ApiResponse Post([FromBody] Establecimientos obj)
+        {
+            return obj.Insert();
+        }
+
+        // PUT api/<EstablecimientosController>/5
+        [HttpPut("{id}")]
+        public ApiResponse Put(int id, [FromBody] Establecimientos obj)
+        {
+            return obj.Update(id);
+        }
+
+        // DELETE api/<EstablecimientosController>/5
+        [HttpDelete("{id}")]
+        public ApiResponse Delete(int id)
+        {
+            return new Establecimientos().Delete(id);
+        }
+    }
+}
---
+++ AppEstablecimientos/Models/Establecimientos.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppEstablecimientos.Models
+{
+    public class Establecimientos
+    {
+        string ConnectionString = "Server=tcp:azurefinal.database.windows.net,1433;Initial Catalog=azureFinal;Persist Security Info=False;User ID=cjamit;Password=Cesar123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public int IDEstablecimiento { get; set; }
+        public string Nombre { get; set; }
+        public string Giro { get; set; }
+        public string Horario { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string PictureBase64 { get; set; }
+        
+
+        public List<Establecimientos> GetAll()
+        {
+            List<Establecimientos> list = new List<Establecimientos>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    string tsql = "getAll";
+                    using (SqlCommand cmd = new SqlCommand(tsql, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                list.Add(new Establecimientos
+                                {
+                                    IDEstablecimiento = (int)reader["id"],
+                                    Nombre = reader["nombre"].ToString(),
+                                    Giro = reader["giro"].ToString(),
+                                    Horario = reader["horario"].ToString(),
+                                    Latitude = (double)reader["latitude"],
+                                    Longitude = (double)reader["longitude"],
+                                    PictureBase64 = reader["foto"].ToString()
+
+                                });
+                            }
+                        }
+                    }
+                }
+                return list;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Establecimientos Get(int id)
+
+        {
+            Establecimientos est = new Establecimientos();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    string tsql = "getById";
+                    using (SqlCommand cmd = new SqlCommand(tsql, conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                est = new Establecimientos
+                                {
+                                    IDEstablecimiento = (int)reader["id"],
+                                    Nombre = reader["nombre"].ToString(),
+                                    Giro = reader["giro"].ToString(),
+                                    Horario = reader["horario"].ToString(),
+                                    Latitude = (double)reader["latitude"],
+                                    Longitude = (double)reader["longitude"],
+                                    PictureBase64 = reader["foto"].ToString()
+                                };
+                            }
+                        }
+                    }
+                }
+                return est;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ApiResponse Insert()
+        {
+            object id;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("CreateEstablecimiento", conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@nombre", Nombre);
+                        cmd.Parameters.AddWithValue("@giro", Giro);
+                        cmd.Parameters.AddWithValue("@horario", Horario);
+                        cmd.Parameters.AddWithValue("@latitude", Latitude);
+                        cmd.Parameters.AddWithValue("@longitude", Longitude);
+                        cmd.Parameters.AddWithValue("@foto", PictureBase64);
+                        id = cmd.ExecuteNonQuery();
+                    }
+                }
+                if (id != null && id.ToString().Length > 0)
+                {
+                    return new ApiResponse
+                    {
+                        IsSuccess = true,
+                        Result = int.Parse(id.ToString()),
+                        Message = "Establecimiento Creado"
+                    };
+                    
+                }
+                else
+                {
+                    return new ApiResponse
+                    {
+                        IsSuccess = false,
+                        Result = 0,
+                        Message = "ERROR"
+                    };
+                   
+                }
+            }
+            catch (Exception exc)
+            {
+                return new ApiResponse
+                {
+                    IsSuccess = false,
+                    Result = null,
+                    Message = exc.Message
+                };
+                throw;
+            }
+        }
+
+        public ApiResponse Update(int id)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand("UpdateEstablecimiento", conn))
+                    {
+                        
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@id", IDEstablecimiento);
+                        cmd.Parameters.AddWithValue("@nombre", Nombre);
+                        cmd.Parameters.AddWithValue("@giro", Giro);
+                        cmd.Parameters.AddWithValue("@horario", Horario);
+                        cmd.Parameters.AddWithValue("@latitude", Latitude);
+                        cmd.Parameters.AddWithValue("@longitude", Longitude);
+                        cmd.Parameters.AddWithValue("@foto", PictureBase64);
+                        id = cmd.ExecuteNonQuery();
+                    }
+                }
+                return new ApiResponse
+                {
+                    IsSuccess = true,
+                    Result = IDEstablecimiento,
+                    Message = "Establecimiento actualizado"
+                };
+            }
+            catch (Exception exc)
+            {
+                return new ApiResponse
+                {
+                    IsSuccess = false,
+                    Result = null,
+                    Message = exc.Message
+                };
+                throw;
+            }
+        }
+
+        public ApiResponse Delete(int id)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    string tsql = "DeleteEstablecimiento";
+
+                    using (SqlCommand cmd = new SqlCommand(tsql, conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return new ApiResponse
+                {
+                    IsSuccess = true,
+                    Result = id,
+                    Message = "Establecimiento eliminado"
+                };
+            }
+            catch (Exception exc)
+            {
+                return new ApiResponse
+                {
+                    IsSuccess = false,
+                    Result = null,
+                    Message = exc.Message
+                };
+                throw;
+            }
+        }
+    }
+}
+    
